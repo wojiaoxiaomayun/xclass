5674e991f723e8995b54b5678c05abad46c739b4
wojiaoxiaomayun <1146897508@qq.com>
2023-03-09 12:40:59 +0800

    update version

--- playground/vue2-test/App.vue
+++ playground/vue2-test/App.vue
@@ -1,5 +1,7 @@
 <template>
-    <div v-xclass class="padding-16 radius-4 bg-primary">aa</div>
+<div>
+    <div v-xclass v-for="i in 10000" :key="i" class="padding-16 radius-4 bg-primary">aa</div>
+</div>
 </template>
 <script>
     export default{
--- playground/vue2-test/main.js
+++ playground/vue2-test/main.js
@@ -1,7 +1,13 @@
 import Vue from "vue";
 import App from './App.vue'
 import xclass from 'xclass-vue'
-Vue.use(xclass)
+Vue.use(xclass,{
+  debug:true,
+  pseudoClassDefine:{
+      'hov:':':hover'
+  },
+  initialRenderNum:-1
+})
 new Vue({
   render:h => h(App)
 }).$mount('#app')
